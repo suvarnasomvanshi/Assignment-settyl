6b068834528e14b3c31a685d569baa7e2a2e2507
suvarna <somavanshisuvarna48@gmail.com>
2023-10-27 17:39:07 +0530

    creating model

---
+++ server/Model/model.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+
+
+const Schema = mongoose.Schema;
+
+
+const itemListSchema = new Schema({
+
+    id:{
+        type : Number,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    quantity:{
+        type:Number,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    image:{
+        type:String,
+        required: true,
+    },
+
+})
+
+
+
+
+
+
+const userSchema = new Schema({
+    
+    name:{
+        type:String,
+        required: true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        length:8,
+    },
+    itemList:[itemListSchema],
+
+    auctionItem :{
+      
+    },
+    transctionhistory:[{
+        date:{
+            type:Date,
+            required: true,
+        },
+        description :String,
+        amount: Number,
+    }]
+
+
+})
+
+export default mongoose.model('User',userSchema)
+
