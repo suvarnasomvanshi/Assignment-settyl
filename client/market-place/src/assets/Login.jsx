3795a4ff74a85d9eea4bce20640c51a685cc88bc
suvarna <somavanshisuvarna48@gmail.com>
2023-10-28 22:24:46 +0530

    login page

--- client/market-place/src/assets/Login.jsx
+++ client/market-place/src/assets/Login.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import { TextField, Box, Button, Typography } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const sendRequest = async () => {
+    const res = await axios
+      .post("http://localhost:8000/api/login", {
+        email: inputs.email,
+        password: inputs.password,
+      })
+      .catch((err) => console.log(err));
+
+    const data = await res.data;
+    console.log(data);
+    return data;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendRequest().then(() => navigate("/home"));
+  };
+
   return (
     <div>
-      login
-    </div>
-  )
-}
+      <form onSubmit={handleSubmit}>
+        <Box
+          display="flex"
+          flexDirection={"column"}
+          justifyContent="center"
+          alignItems="centre"
+          width={300}
+          marginLeft="auto"
+          marginRight="auto"
+        >
+          <Typography variant="h3">login</Typography>
 
-export default Login
+          <TextField
+            margin="normal"
+            variant="outlined"
+            placeholder="email"
+            name="email"
+            type="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            variant="outlined"
+            placeholder="password"
+            name="password"
+            type="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+
+          <Button variant="contained" type="submit">
+            Login
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
