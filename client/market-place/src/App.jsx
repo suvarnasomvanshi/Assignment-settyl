d1eddbb53da49f883a450d3a7a4ba37e2d5b1b77
suvarna <somavanshisuvarna48@gmail.com>
2023-10-28 19:50:14 +0530

    installing mui, create navbar

--- client/market-place/src/App.jsx
+++ client/market-place/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-
+import Headers from './assets/Headers'
 
 function App() {
- 
-
+  
   return (
     <>
-    <div>wsdhwuy</div>
-    
+        <div><Headers/></div>
     </>
   )
 }
---
+++ client/market-place/src/assets/Headers.jsx
@@ -0,0 +1,32 @@
+import React,{useState} from 'react'
+import {AppBar,Toolbar,Typography,Box, Tabs, Tab} from "@mui/material"
+
+const Headers = () => {
+    const [value,setValue] = useState();
+
+  return (
+    <div>
+          <AppBar>
+            <Toolbar>
+                <Typography variant="h4">MarketPlace</Typography>
+                <Box sx={{
+                    marginLeft:'auto'
+                }}>
+                    <Tabs 
+                    onChange={(e,val)=>setValue(val)} 
+                    value={value} 
+                    textColor='inherit'
+                    indicatorColor='secondary'
+                    >
+                        <Tab label="Login" />
+                        <Tab label="SignUp" />
+                        <Tab label="Logint" />
+                    </Tabs>
+                </Box>
+            </Toolbar>
+          </AppBar>
+    </div>
+  )
+}
+
+export default Headers
